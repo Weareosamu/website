8316431d9c690dbb18f08b74811877d29cce1f66
Weareosamu <123486368+Weareosamu@users.noreply.github.com>
2023-03-12 20:14:35 +0100

    Update scripts.js

--- watchtoearn/scripts.js
+++ watchtoearn/scripts.js
@@ -95,6 +95,7 @@ function updateTimer() {
 
     // Update the token count in the Firebase database using transaction()
 const wallet = walletParam; // Use the wallet parameter from the URL
+    console.log(" ${wallet} ");
 const tokenRef = database.ref('users/' + wallet + '/token');
 tokenRef.transaction(function(currentTokenCount) {
   if (currentTokenCount === null || currentTokenCount === undefined) {
