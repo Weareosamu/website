db7b1b1c26fd06bcec2d60fe78ebda986b488fe0
Weareosamu <123486368+Weareosamu@users.noreply.github.com>
2023-03-13 18:18:01 +0100

    Update scripts.js

--- watchtoearn/scripts.js
+++ watchtoearn/scripts.js
@@ -1,170 +1,107 @@
+(function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyAxZqRlbFKnvLXQfDA5H-HdiPBLEjAlbXM",
+    authDomain: "watchtoearn-3a2ac.firebaseapp.com",
+    databaseURL: "https://watchtoearn-3a2ac-default-rtdb.firebaseio.com",
+    projectId: "watchtoearn-3a2ac",
+    storageBucket: "watchtoearn-3a2ac.appspot.com",
+    messagingSenderId: "1018286487266",
+    appId: "1:1018286487266:web:c08335e403ae893bfa10bb",
+    measurementId: "G-L809K7DPGK"
+  };
 
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAxZqRlbFKnvLXQfDA5H-HdiPBLEjAlbXM",
-  authDomain: "watchtoearn-3a2ac.firebaseapp.com",
-  databaseURL: "https://watchtoearn-3a2ac-default-rtdb.firebaseio.com",
-  projectId: "watchtoearn-3a2ac",
-  storageBucket: "watchtoearn-3a2ac.appspot.com",
-  messagingSenderId: "1018286487266",
-  appId: "1:1018286487266:web:c08335e403ae893bfa10bb",
-  measurementId: "G-L809K7DPGK"
-};
+  function displayTokenCount() {
+    // Get wallet from URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const wallet = urlParams.get("wallet");
+    if (!wallet) {
+      return;
+    }
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-
-function displayTokenCount() {
-  // Get wallet from URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const wallet = urlParams.get("wallet");
-  if (!wallet) {
-    return;
-  }
-
-  const tokenRef = database.ref("users/" + wallet + "/token");
-  tokenRef.on("value", function (snapshot) {
-    const tokenCount = snapshot.val() || 0;
-    const countElement = document.querySelector("#tokenDisplay .count");
-    countElement.textContent = tokenCount;
-  });
-}
-
-////////////////////////////////////////////////////////////////TIMER
-
-let startTime, elapsedTime, timerInterval;
-
-const form = document.querySelector('#tokenForm');
-const timer = document.querySelector('#timer');
-
-// Extract the wallet parameter from the URL
-const params = new URLSearchParams(window.location.search);
-const walletParam = params.has("wallet") ? params.get("wallet") : null;
-
-let tokenCount = 0.0;
-let tokenCountElement = document.getElementById('tokenCount');
-
-
-function updateTimer() {
-  // Get the elapsed time
-  elapsedTime = Date.now() - startTime;
-
-  // Format the time into hours, minutes, and seconds
-  let hours = Math.floor(elapsedTime / 3600000);
-  let minutes = Math.floor((elapsedTime % 3600000) / 60000);
-  let seconds = Math.floor((elapsedTime % 60000) / 1000);
-
-  // Add leading zeros to minutes and seconds if necessary
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
-
-  // Update the timer display
-  timer.textContent = `${hours}:${minutes}:${seconds}`;
-
-  // Check if a minute has passed and add 0.5 token if so
-  if (elapsedTime >= 60000) {
-
-    // Update the token count in the Firebase database
-    const wallet = walletParam; // Use the wallet parameter from the URL
-    const walletRef = database.ref('users/' + wallet);
-
-    // Create the wallet section if it doesn't exist yet
-    walletRef.once('value', function(snapshot) {
-      if (!snapshot.exists()) {
-        walletRef.set({ token: 0 });
-        console.log(wallet); // log the wallet value to the console
-      } else {
-        // Retrieve the current token count and update it
-        const currentTokenCount = snapshot.child('token').val();
-        const newTokenCount = (currentTokenCount === null || currentTokenCount === undefined) ? 1 : currentTokenCount + 0.5;
-        walletRef.update({ token: newTokenCount });
-        console.log(wallet); // log the wallet value to the console
-      }
+    const tokenRef = database.ref("users/" + wallet + "/token");
+    tokenRef.on("value", function (snapshot) {
+      const tokenCount = snapshot.val() || 0;
+      const countElement = document.querySelector("#tokenDisplay .count");
+      countElement.textContent = tokenCount;
     });
-
-    // Reset the start time
-    startTime += 60000;
   }
 
-  // Update the token count element in the HTML
-  displayTokenCount();
-}
+  // Extract the wallet parameter from the URL
+  const params = new URLSearchParams(window.location.search);
+  const walletParam = params.has("wallet") ? params.get("wallet") : null;
 
+  let startTime, elapsedTime, timerInterval;
+  const form = document.querySelector('#tokenForm');
+  const timer = document.querySelector('#timer');
 
-////////////////////////////////////////////////////////////////TIMEREND
+  let tokenCountElement = document.getElementById('tokenCount');
 
+  function updateTimer() {
+    // Get the elapsed time
+    elapsedTime = Date.now() - startTime;
 
-function submitHandler(event) {
-  event.preventDefault();
+    // Format the time into hours, minutes, and seconds
+    let hours = Math.floor(elapsedTime / 3600000);
+    let minutes = Math.floor((elapsedTime % 3600000) / 60000);
+    let seconds = Math.floor((elapsedTime % 60000) / 1000);
 
-  // Get user's wallet and email input values
-  const emailInput = document.querySelector("#email");
-  const walletInput = document.querySelector("#wallet");
-  const email = emailInput.value;
-  const wallet = walletInput.value;
+    // Add leading zeros to minutes and seconds if necessary
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
 
-  // Return if either email or wallet is empty
-  if (email === "" || wallet === "") {
-    return;
-  }
+    // Update the timer display
+    timer.textContent = `${hours}:${minutes}:${seconds}`;
 
-  // Check if email and wallet parameters exist
-  const urlParams = new URLSearchParams(window.location.search);
-  const urlEmail = urlParams.get("email");
-  const urlWallet = urlParams.get("wallet");
+    // Check if a minute has passed and add 0.5 token if so
+    if (elapsedTime >= 60000) {
 
-  // Add email and wallet to URL
-  urlParams.set("email", email);
-  urlParams.set("wallet", wallet);
-  window.history.replaceState({}, "", "?" + urlParams.toString());
-
-  // Get the start time
+      // Update the token count in the Firebase database
+const wallet = walletParam; // Use the wallet parameter from the URL
+const tokenRef = database.ref("users/" + wallet + "/token");
+tokenRef.transaction(function (currentTokenCount) {
+return (currentTokenCount || 0) + 0.5;
+});
+        // Reset the start time and update the token count display
   startTime = Date.now();
-  // Start the timer interval
-  timerInterval = setInterval(updateTimer, 1000);
+  const tokenCount = parseFloat(tokenCountElement.textContent);
+  tokenCountElement.textContent = (tokenCount + 0.5).toFixed(1);
+}
+    }
 
-  // Authenticate anonymously
-  firebase
-    .auth()
-    .signInAnonymously()
-    .then(() => {
-      // Write new data to database or update existing data
-      const currentTime = new Date().getTime();
-      const emailRef = database.ref("users/" + wallet);
-      emailRef.once("value", (snapshot) => {
-        const data = snapshot.val();
-        if (data && data.email === email) {
-          // User already exists, don't add any tokens
-          emailRef.update({
-            timestamp: currentTime,
-          });
-          console.log("Existing user signed in, data updated in database successfully!");
-        } else {
-          // New user, add 0 tokens
-          emailRef.set({
-            email: email,
-            token: 0,
-            timestamp: currentTime,
-          });
-          console.log("New user signed up, data written to database successfully!");
-        }
-      });
-      // Clear input fields
-      emailInput.value = "";
-      walletInput.value = "";
-    })
-    .catch((error) => {
-      // Handle authentication error
-      console.log("Authentication failed:", error.message);
-    });
+function startTimer() {
+startTime = Date.now();
+timerInterval = setInterval(updateTimer, 1000);
 }
 
+function stopTimer() {
+clearInterval(timerInterval);
+}
 
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+startTimer();
+});
 
-const submitBtn = document.querySelector('#tokenForm input[type="submit"]');
-submitBtn.addEventListener('click', submitHandler);
+tokenCountElement.addEventListener('click', function() {
+// Update the token count in the Firebase database
+const wallet = walletParam; // Use the wallet parameter from the URL
+const tokenRef = database.ref("users/" + wallet + "/token");
+tokenRef.transaction(function (currentTokenCount) {
+return (currentTokenCount || 0) + 1;
+});
+// Update the token count display
+const tokenCount = parseFloat(tokenCountElement.textContent);
+tokenCountElement.textContent = (tokenCount + 1).toFixed(1);
+  
+});
+
+displayTokenCount();
+})();
