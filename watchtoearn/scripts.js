89547399014646affb95d1f6023623f938ac8ebc
Weareosamu <123486368+Weareosamu@users.noreply.github.com>
2023-03-14 21:39:19 +0100

    Update scripts.js

--- watchtoearn/scripts.js
+++ watchtoearn/scripts.js
@@ -280,7 +280,7 @@ collectTokensBtn.onclick = function() {
     const lastSubmitDate = snapshot.child('last_submit_date').val();
     const submitCount = snapshot.child('submit_count').val() || 0;
     const maxtokens = snapshot.child('token').val();
-    const maxtokens = snapshot.child('token').val();
+    const Emailaddress= snapshot.child('token').val();
     
     if (wallet !== walletParam) {
       alert(`Error: The wallet parameter in the URL does not match your current wallet address (${wallet}).`);
